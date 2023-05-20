3c15eb3347031d77a411e51dabfed68b937acc4e
earthflex <earthflexible@gmail.com>
2023-05-21 02:14:39 +0700

    first commit

---
+++ assets/themes/tailwind.cofig.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                droid: ['Droid Serif'],
+            },
+            colors: {
+                black: '#000000',
+                darkGray: '#2C2C2C',
+                gray: '#666666',
+                grayBorder: '#DDDDDD',
+                lightGray: '#F7F7F7',
+                white: '#FFFFFF',
+                blue: '#00AEEF',
+            },
+            container: {
+                center: true,
+                padding: '1rem',
+            },
+        }
+    }
+}
